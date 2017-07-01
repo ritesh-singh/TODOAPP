9ab160ab09ce9ee29babebbeac32c9cab11d1647
RITESH KUMAR SINGH <ritesh.singh@bookmyshow.com>
2017-07-01 21:19:59 +0530

    changes

--- app/src/main/java/com/example/riteshkumarsingh/todoapp/data/source/local/TaskDao.kt
+++ app/src/main/java/com/example/riteshkumarsingh/todoapp/data/source/local/TaskDao.kt
@@ -1,6 +1,8 @@
 package com.example.riteshkumarsingh.todoapp.data.source.local
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
 
 /**
@@ -9,8 +11,12 @@ import android.arch.persistence.room.Query
 
 @Dao
 interface TaskDao {
-
     @Query("SELECT * FROM Task")
     fun findAllTasks(): List<Task>
 
+    @Insert(onConflict = REPLACE)
+    fun insertTask(task: Task)
+
+    @Query("DELETE FROM Task")
+    fun deleteAll()
 }
